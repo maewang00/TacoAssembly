07f782383bd44abef211b79d5a6df01b6f30c3a7
maewang00 <46982693+maewang00@users.noreply.github.com>
2019-09-29 09:04:55 -0700

    Added comments and information

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,36 @@
-/// Variables and data ///
-// Global list of selected items: updates everytime a taco is submitted 
+/////////////////////////////////////////////INFORMATION AND SOURCE////////////////////////////////////////////////////////
+//	Designed and created by: Mae Wang
+//  API Source: https://ct-tacoapi.azurewebsites.net
+//	
+//	Assumptions and Protocols:
+//		1. A taco is strictly defined as having 1 shell, 1 base layer, 1 mixin, 1 condiment, and 1 seasoning, at minimum.
+//		2. No threshold limitations are added for stretch goal 3. Users are assumed to put however much they like. 
+//		   No taco-lover should ever be limited!
+//		3. Random taco mode gives a taco of exactly 1 shell, 1 base layer, 1 mixin, 1 condiment, and 1 seasoning.
+//		4. To remove a submitted taco from the list, hover over a taco description and click on it. A confirmation pop-up will appear.
+//		5. To add ingredients to a taco, hover over the selections under "Ingredients" on the right-hand side and click on an ingredient.
+//		   To deselect an ingredient, re-click the selection again.
+//		6. Navigate between buttons 'Make Random', 'Make My Own', and 'Generate Taco' accordingly.
+//		7. 'Make Random' is for generating randomized tacos when the 'Generate Taco' button is pressed.
+//		8. 'Make My Own' is for generating user-defined tacos when the 'Generate Taco' button is pressed. Options under 'Ingredients'
+//		   must be clicked and validated to generate a taco.
+//
+//	Build Instructions:	
+//		1. First, the layout of the HTML file was created with the appropriate divs, classes, and ids for hierarchy. 
+//		2. Next, the CSS file was implemented with a clear design in mind. Here, the front-end was complete and was 
+//		   slightly modified during the process of implementing the back-end.
+//		3. Then, basic animations and small action listener interactions were implemented on buttons using JQuery. There was a minor preference 
+//		   to use JavaScript on the submission button action listener function.
+//		4. JavaScript was used for the rest of the process in script.js. JSON parsing and storing was implemented for future access.
+//		5. The 'Make My Own' functionalities were implemented using multiple functions that interlinked with each other. Action listener responses were
+//		   dealt with JQuery and JavaScript DOM methods.
+//		6. The 'Make Random' functionalities were implemented using existing 'Make My Own' functions. Dictionaries/Objects were mainly used for data storage.
+//		7. Lastly, deletion properties and bugs were fixed. Program was tested thoroughly for obvious bugs.
+//		
+////////////////////////////////////////// Variables and data lists ///////////////////////////////////////////////////////
+
+// Global lists of selected items:
+// Updates everytime an ingredient was selected or a taco is submitted.
 var currItemsList = {
 	shells: [],
 	baseLayers: [],
@@ -8,6 +39,7 @@ var currItemsList = {
 	seasonings: []
 };
 
+// A collection of the entire JSON files in one location (for randomization).
 var allItemsList = {
 	shells: [],
 	baseLayers: [],
@@ -16,6 +48,7 @@ var allItemsList = {
 	seasonings: []
 };
 
+// Randomized data for random tacos. Updates everytime a random taco is submitted.
 var generatedList = {
 		shells: [],
 		baseLayers: [],
@@ -26,11 +59,14 @@ var generatedList = {
 
 // Variable that update modes in JQuery.
 var randomMode = true;
-// Variable to make ids for made tacos.
+
+// Variable to make ids for submitted tacos.
 var idCount = 0;
 
 // Categories for ingredients.
 var categories = ["shells", "baseLayers", "mixins", "condiments", "seasonings"];
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // When submit button is clicked, add taco!
 window.onload = function() {
@@ -47,7 +83,7 @@ window.onload = function() {
 	}
 };
 
-//Clears ingredient list after making a taco.
+// Clears ingredient lists and color markers after making a taco.
 function clearItemsList() {
 	currItemsList = {
 		shells: [],
@@ -70,22 +106,21 @@ function clearItemsList() {
 		elements[i].style.backgroundColor = "#A4FCA0";
 	}
 };
-
+// Generates one random ingredient per section, makes the taco, and publishes it immediately.
 function addRandomTaco() {
 	for (key in allItemsList) {
 		var arr = allItemsList[key];
 		generatedList[key].push(arr[Math.floor(Math.random()*arr.length)]);
 	}
-
 	var newTaco = makeTaco(generatedList, "random-tacos");
 };
 
-//Assuming that the criteria of taco is fulfilled. Add to tacoIdDict
+// Generates a build-your-own-taco, assuming that the criteria of taco is fulfilled. Taco is published immediately.
 function addUserTaco() {
 	makeTaco(currItemsList, "user-made-tacos");
 };
 
-// Generic taco object maker.
+// Generic taco object maker. This function does the publishing.
 function makeTaco(itemsList, category) {
 	idCounter = idCount;
 	const taco = {
@@ -112,10 +147,12 @@ function makeTaco(itemsList, category) {
 	return taco;
 };
 
+// Generates the general taco sentence output.
 function getTacoSentence(taco) {
 	return "A taco consisting of: " + taco.shells + taco.baseLayers + taco.mixins + taco.condiments + taco.seasonings;
 };
 
+// Helper function for getTacoSentence(taco) by adding punctuation and returning a string of an ingredient category.
 function stringify(arrOfIng) {
 	var arrayLength = arrOfIng.length;
 
@@ -134,14 +171,7 @@ function stringify(arrOfIng) {
 	return builder;
 };
 
-function stringifyLast(arrayLength, arrOfIng, lst) {
-	var builder = "";
-		for (var i = 0; i < arrayLength - 1; i++) {
-			builder += arrOfIng["seasonings"][i] + ", ";
-		}
-		builder += "and " + arrOfIng["seasonings"][arrayLength - 1] + ".";
-}
-
+// Validates the user's selections in ingredients and returns a boolean.
 function checkValidTaco() {
 	for (key in currItemsList) {
 		if (currItemsList[key].length == 0) {
@@ -152,15 +182,17 @@ function checkValidTaco() {
 	return true;
 };
 
+// Removes the taco with a tid (taco ID) from a list
 function deleteTaco(tid) {
 	document.getElementById(tid).remove();
-}
+};
 
-// some unique taco key
+// Returns a unique taco key tid
 function getTacoID(taco){
   return taco.tid;
 };
 
+// Removes ingredient from the tracking list if the user deselects an ingredient item.
 function removeIngredient(sect, ingName) {
 	var partsArray = sect.split(' ');
 	var section = partsArray[1];
@@ -170,12 +202,14 @@ function removeIngredient(sect, ingName) {
 	}
 };
 
+// Adds ingredient to the tracking list if the user selects an ingredient item.
 function addIngredient(sect, ingName) {
 	var partsArray = sect.split(' ');
 	var section = partsArray[1];
 	currItemsList[section].push(ingName);
 };
 
+// Generic equals() function.
 function arraysEqual(a, b) {
   if (a === b) return true;
   if (a == null || b == null) return false;
@@ -184,8 +218,9 @@ function arraysEqual(a, b) {
     if (a[i] !== b[i]) return false;
   }
   return true;
-}
+};
 
+// Runs all ClickTime taco API url requests
 categories.forEach(function (item, index) {
   getJSONfile('https://ct-tacoapi.azurewebsites.net/' + item, function(err, data) {
   if (err !== null) {
@@ -196,7 +231,7 @@ categories.forEach(function (item, index) {
 }, item);
 });
 
-// Function to manage JSON url requests
+// Function to manage JSON url requests.
 function getJSONfile(url, callback, category) {
 	var request = new XMLHttpRequest()
 	request.open('GET', url, true);
@@ -227,9 +262,8 @@ function getJSONfile(url, callback, category) {
 };
 
 
-//JQuery basic animation functionality for the action buttons.
+// JQuery basic animation functionality for the action buttons.
 $(document).ready(function() {
- 	$("#error-displayer").hide();
  	$("#activity-container-right").hide();
  	$("#activity-container-left").css("width", "100%");
 
